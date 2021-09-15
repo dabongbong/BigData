6f892b3a820fa6fff5a528d454e063ccb8cc00da
dabongbong <killers61@icloud.com>
2021-09-15 16:57:59 +0900

    0915 수업끝

--- homework/test.py
+++ homework/test.py
@@ -1,1 +1,16 @@
-print("커밋 테스트합니당/")
+head = 0
+tail = 6
+array = [11, 13, 17, 19, 23, 29, 31]
+
+while True:
+    mid = (head+tail)/2
+    if array[mid] < 17:
+        head = mid + 1
+    elif array[mid] > 17:
+        tail = mid - 1
+    else:
+        print("{}번째 요소가 동일".format(mid))
+        break
+    if head <= tail:
+        print("찾지 못했습니다.")
+        break
