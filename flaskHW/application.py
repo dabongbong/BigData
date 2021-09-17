a5f2027d1ef8d5b27ce0438443e47ebd975d4834
dabongbong <killers61@icloud.com>
2021-09-17 19:30:24 +0900

    커밋

---
+++ flaskHW/application.py
@@ -0,0 +1,34 @@
+from flask import url_for, session, Flask, render_template, request, redirect
+
+application = Flask(__name__)
+application.secret_key = "dabongbong"
+
+ID = "hello"
+PW = "world"
+
+@application.route("/")
+def home():
+    if "userID" in session:
+        return render_template("home.html", username = session.get("userID"), login = True)
+    else: 
+        return render_template("home.html", login=False)
+@application.route("/login", methods = ["get"])
+def login():
+    global ID, PW
+    _id_ = request.args.get("loginId")
+    _password_ = request.args.get("loginPw")
+
+    if ID == _id_ and _password_ == PW:
+        session["userID"] = _id_
+        return redirect(url_for("home"))
+    else:
+        return redirect(url_for("home"))
+
+
+
+@application.route("/logout")
+def logout():
+    session.pop("userID")
+    return redirect(url_for("home"))
+
+application.run(host= "0.0.0.0")
